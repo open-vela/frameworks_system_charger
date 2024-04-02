576e333ba769895aef2a3b7a650abbf2dc6e461c
dulibo1 <dulibo1@xiaomi.com>
2024-04-02 15:51:15 +0800

    charger:sync charge state to gauge
    Signed-off-by: dulibo1 <dulibo1@xiaomi.com>

--- include/charger_hwintf.h
+++ include/charger_hwintf.h
@@ -44,4 +44,7 @@ int get_battery_temp(struct charger_manager* manager, int* val);
 int get_battery_current(struct charger_manager* manager, int* cur);
 int get_battery_status(struct charger_manager* manager, enum battery_status_e* state);
 int set_battery_vbus_state(struct charger_manager* manager, bool enable);
+#ifdef CONFIG_CHARGERD_SYNC_CHARGE_STATE
+int set_battery_charge_state(struct charger_manager* manager, unsigned int state);
+#endif
 #endif
