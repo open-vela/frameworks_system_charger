ce0744ea1437fd24b180d628d9fcdbe58eb7fef9
dongjiuzhu1 <dongjiuzhu1@xiaomi.com>
2024-10-24 21:45:07 +0800

    frameworks/charged: can't stop charger when fullbatt_capacity is 100.
    When the battery is fully charged, the hardware will automatically stop
    charging. For cases where the battery is not fully charged to 100%, the
    software needs to actively stop the charging process.
    
    Signed-off-by: dongjiuzhu1 <dongjiuzhu1@xiaomi.com>

--- charger_statemachine.c
+++ charger_statemachine.c
@@ -63,7 +63,8 @@ static bool check_battery_full(struct charger_manager* manager)
     }
 
     chargerdebug("capacity :%d current:%d\n", capacity, current);
-    if (capacity >= manager->desc.fullbatt_capacity && current >= 0 && current <= manager->desc.fullbatt_current) {
+    if (capacity >= manager->desc.fullbatt_capacity && manager->desc.fullbatt_capacity != 100 &&
+        current >= 0 && current <= manager->desc.fullbatt_current) {
 
         /*three times of continuous,the condition is satisfying, avoid jitter */
 
